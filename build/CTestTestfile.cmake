96b23d76f508f1dbcd27c7bc1ef2415da0ece8af
HongweiSunny <2043500880@qq.com>
2022-04-10 14:28:59 +0800

    delete build

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shw/catkin_ws_sakichen/src/apa_planner
-# Build directory: /home/shw/catkin_ws_sakichen/src/apa_planner/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
